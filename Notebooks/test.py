cbd5760029961af69e6cf45c6df53f13d7f4786a
Anthony Hackett <59706388+h-ckett@users.noreply.github.com>
2020-01-30 13:55:30 -0500

    add test.py
    make sure to edit the driver code to test both methods and to reproduce the problem discussed here: https://stackoverflow.com/questions/59990709/scikit-image-threshold-multiotsu-outputting-black-frames-from-video

---
+++ Notebooks/test.py
@@ -0,0 +1,48 @@
+# imports start
+import cv2
+import numpy as np
+from skimage.filters import threshold_multiotsu
+# imports end
+
+# frameCapture start
+# Function to extract frames
+def frameCapture(fc_path):
+    fc_vidObj = cv2.VideoCapture(fc_path) # Path to video file
+    fc_count = 0                          # Used as counter variable
+    fc_success = 1                        # Checks if the frames were extracted
+    
+    while fc_success:
+        # vidObj object calls read
+        fc_success, fc_image = fc_vidObj.read()
+        fc_image = cv2.cvtColor(fc_image, cv2.COLOR_BGR2GRAY) # Convert the image to grayscale
+        fc_image = cv2.GaussianBlur(fc_image,(5,5),0) # Gaussian noise filtering
+        (threshold, fc_image) = cv2.threshold(fc_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) # Normalize & Segment
+        cv2.imwrite("frame%d.jpg" % fc_count, fc_image) # Saves the frames
+        fc_count += 1
+# frameCapture end
+        
+# frameCaptureMulti start
+# Function to extract frames [multi-otsu thresholding]
+def frameCaptureMulti(fc_path):
+    fc_vidObj = cv2.VideoCapture(fc_path) # Path to video file
+    fc_count = 0                          # Used as counter variable
+    fc_success = 1                        # Checks if the frames were extracted
+    
+    while fc_success:
+        # vidObj object calls read
+        fc_success, fc_image = fc_vidObj.read()
+        fc_image = cv2.cvtColor(fc_image, cv2.COLOR_BGR2GRAY) # Convert the image to grayscale
+        fc_image = cv2.GaussianBlur(fc_image,(5,5),0) # Gaussian noise filtering
+        fc_thresholds = threshold_multiotsu(fc_image)
+        fc_regions = np.digitize(fc_image, bins=fc_thresholds)
+        cv2.imwrite("frame%d.jpg" % fc_count, fc_regions) # Saves the frames
+        fc_count += 1
+# frameCaptureMulti end
+
+# driver code start
+# Testing on a mouse video
+frameCapture("C:\\Users\\ahack\\Documents\\QC Lab Docs\\David Mouse Project\\1-8-20\\REC_0001.MP4")
+# driver code end
+
+# Note: frameCapture() functions fine, but frameCaptureMulti() just outputs black frames...
+# The issue is not with the grayscale conversion or the gaussian filtering (both tested)
